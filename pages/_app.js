8a1ce2c336d16c97e5fd8425dc25bf2783ba272a
joezak-outta <jkamardo@gmail.com>
2020-08-16 23:36:03 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import React from "react";
+import "../styles/global.css"
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
